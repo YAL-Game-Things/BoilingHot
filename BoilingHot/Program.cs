a090bb897a4348c3bdd8718ae0c61ee2b826025f
YellowAfterlife <yellowafterlife@ymail.com>
2019-02-17 01:56:10 +0200

    There's not much to this

---
+++ BoilingHot/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
+
+namespace BoilingHot {
+    class Program {
+        static FieldDefinition GetField(ModuleDefinition module, string type, string name) {
+            return module.GetType(type).Fields.First(m => m.Name == name);
+        }
+        static MethodDefinition GetMethod(ModuleDefinition module, string type, string name) {
+            return module.GetType(type).Methods.First(m => m.Name == name);
+        }
+        static void PatchUpdate(ModuleDefinition module) {
+            var update = GetMethod(module, "Unexplored.Core.Grid.DungeonGrid", "Update");
+            var processor = update.Body.GetILProcessor();
+            var instructions = update.Body.Instructions;
+            //
+            var insertAt = -1;
+            for (var i = 0; i < instructions.Count; i++) {
+                var instruction = instructions[i];
+                if (instruction.OpCode.Code != Code.Ldfld) continue;
+                var field = instruction.Operand as FieldReference;
+                if (field == null || field.Name != "Slowed") continue;
+                if (field.DeclaringType.FullName != "Unexplored.Core.PlayState") continue;
+                insertAt = i;
+                break;
+            }
+            if (insertAt < 0) throw new Exception("Couldn't find where to put the new logic at in DungeonGrid.Update");
+            //
+            var jumpIf = instructions[insertAt + 1];
+            if (jumpIf.OpCode != OpCodes.Brfalse_S) throw new Exception("It would appear like this executable is already patched");
+            var locTrue = instructions[insertAt + 2];
+            var locFalse = jumpIf.Operand as Instruction;
+            var gm_instance = GetField(module, "Unexplored.Core.GameManager", "Instance");
+            var gm_player = GetField(module, "Unexplored.Core.GameManager", "Player");
+            //
+            processor.Replace(jumpIf, processor.Create(OpCodes.Brtrue_S, locTrue));
+            // lurge <= 0:
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldsfld, gm_instance));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld, gm_player));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld,
+                GetField(module, "Unexplored.Core.Mobs.Creature", "lurge")));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldc_R4, 0f));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Bgt_Un_S, locFalse));
+            // walk <= 0:
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldsfld, gm_instance));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld, gm_player));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld,
+                GetField(module, "Unexplored.Core.Mobs.Creature", "Walk")));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldc_R4, 0f));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Bgt_Un_S, locFalse));
+            // LookAheadFactor <= 0:
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldsfld, gm_instance));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld, gm_player));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldfld,
+                GetField(module, "Unexplored.Core.Mobs.Player", "LookAheadFactor")));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Ldc_R4, 0f));
+            processor.InsertBefore(locTrue, processor.Create(OpCodes.Bgt_Un_S, locFalse));
+            // we need to ->simplify->optimize-> because some jumps now don't fit into _S range
+            update.Body.SimplifyMacros();
+            update.Body.OptimizeMacros();
+        }
+        static void PatchSubmit(ModuleDefinition module) {
+            // let's not mess up leaderboards with our hot scores
+            var onFindResult = GetMethod(module, "Unexplored.Utils.Leaderboards", "OnLeaderboardFindResult");
+            var processor = onFindResult.Body.GetILProcessor();
+            processor.InsertBefore(onFindResult.Body.Instructions[0], processor.Create(OpCodes.Ret));
+        }
+        static void Main(string[] args) {
+            var dir = ".";
+            var path = dir + "\\Unexplored.exe";
+            var orig = dir + "\\Unexplored-Original.exe";
+            var next = dir + "\\Unexplored-New.exe";
+            if (!File.Exists(path)) {
+                Console.WriteLine("Please extract into the game's directory before running.");
+            } else {
+                try {
+                    Console.WriteLine("Making a backup...");
+                    if (!File.Exists(orig)) File.Copy(path, orig);
+                    //
+                    Console.WriteLine("Patching...");
+                    //
+                    var resolver = new DefaultAssemblyResolver();
+                    resolver.AddSearchDirectory(dir);
+                    var readerParams = new ReaderParameters();
+                    readerParams.AssemblyResolver = resolver;
+                    //
+                    var module = ModuleDefinition.ReadModule(path, readerParams);
+                    PatchUpdate(module);
+                    PatchSubmit(module);
+                    //
+                    Console.WriteLine("Saving...");
+                    module.Write(next);
+                    //
+                    module.Dispose();
+                    module = null;
+                    File.Delete(path);
+                    File.Move(next, path);
+                    //
+                    Console.WriteLine("All good! You can run the game now.");
+                    Console.WriteLine("(and get rid of the patcher's files)");
+                } catch (Exception e) {
+                    Console.WriteLine("An error occurred: " + e);
+                }
+            }
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# BOILING HOT
+This is a small mod that introduces SUPERHOT (or Heat Signature)-esque mechanics to
+[Unexplored](http://store.steampowered.com/app/506870/) - the time only moves when you do!
+
+It is inspired by [a small video](https://twitter.com/PlayUnexplored/status/1095746581779890176)
+from the game's official Twitter account showing some game footage in slow motion.
+This got me thinking "Hey, that looks just about the pace the game goes in Heat Signature during combat.
+I wonder how'd this work as an actual game mechanic". Pretty well, apparently!
+
+Extra links: [itch.io page](https://yellowafterlife.itch.io/boiling-hot) · [small video](https://www.youtube.com/watch?v=sNpEsA_0j4U)
+
+## Mechanics
+Unless you are moving (or trying to), attacking with a melee weapon,
+or using the respective button to look forward, time slows down to 10% of normal.
+
+For ethical reasons, leaderboard submission is disabled while this mod is active.
+
+## Installing
+Extract the binary to the game directory, run BoilingHot executable to patch.
+
+Once that is done, you can remove the mod's files - you won't need them.
+
+## Uninstalling
+Replace Unexplored executable with Unexplored-Original (which the patcher makes for you automatically)
+
+## Technical
+This is a ridiculously simple mod. As in, it took me longer to write a patcher for it than to figure out the actual mod.
+
+There is a condition in the game update loop that goes like this:
+```cs
+if (this.playState.Slowed) {
+    // ease towards 10% gameplay speed
+} else {
+    // ease towards 100% gameplay speed
+}
+```
+and is used for boss intros.
+
+This mod uses [Mono.Cecil](https://github.com/jbevain/cecil/) to patch the condition to be
+```cs
+if (playState.Slowed || (Player.lurge <= 0f && Walk <= 0f && LookAheadFactor <= 0f))
+```
+While I have experimented with making the time stop completely, 10% speed is more than enough for tactical planning,
+and not having to tip-toe around just to let an enemy move a few extra steps feels better. And it's less work too!
